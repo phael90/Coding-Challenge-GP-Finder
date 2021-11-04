e6cbe93061ee614d5d9d54aec8d123ac941bb22c
phael <raphaelugha@gmail.com>
2021-11-04 01:14:03 +0000

    Get user location

--- gp-finder/src/Map/Map.jsx
+++ gp-finder/src/Map/Map.jsx
@@ -1,25 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { GoogleMap, useLoadScript } from "@react-google-maps/api";
 
 const mapContainerStyle = {
   width: "100vw",
   height: "100vh",
 };
-const center = {
-  lat: 55.9550393,
-  lng: -3.1520545,
-};
 
 const libraries = ["places"];
 
 const Map = ({ apiKey }) => {
+  const defaultLocation = {
+    lat: 55.9550393,
+    lng: -3.1520545,
+  };
+  const [center, setCenter] = useState(defaultLocation);
+  const [hasLocation, setHasLocation] = useState(true);
+
+  useEffect(() => {
+    if (navigator?.geolocation) {
+      navigator.geolocation.getCurrentPosition((userLocation) => {
+        setCenter({
+          lat: userLocation.coords?.latitude,
+          lng: userLocation.coords?.longitude,
+        });
+      });
+    } else {
+      setHasLocation(false);
+    }
+  }, []);
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: apiKey,
     libraries,
   });
 
-  if (loadError) return "Error";
-  if (!isLoaded) return "Loading...";
+  if (loadError) return <p>Error</p>;
+  if (!isLoaded) return <p>Loading...</p>;
+  if (!hasLocation) {
+    return <p>Please share your location for best experience</p>;
+  }
 
   return (
     <GoogleMap
@@ -27,6 +45,9 @@ const Map = ({ apiKey }) => {
       mapContainerStyle={mapContainerStyle}
       zoom={8}
       center={center}
+      onClick={(event) => {
+        console.log(event);
+      }}
     ></GoogleMap>
   );
 };
